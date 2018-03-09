42acb0b8f4d5f207f248e75bc6553bb3d51a37d3
PEP\mayz <a976304624@outlook.com>
2018-03-09 20:33:17 +0800

    提交新的  增加权限  修改包名

---
+++ app/src/main/java/com/pep/bintraysecond/MainActivity.java
@@ -0,0 +1,18 @@
+package com.pep.bintraysecond;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iflytek.cloud.SpeechUtility;
+
+import iflytek.speech.util.FaceRect;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FaceRect da;
+    }
+}
---
+++ mylibrary/src/main/java/com/iflytek/ise/result/FinalResult.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.iflytek.ise.result;
+
+/**
+ * <p>Title: FinalResult</p>
+ * <p>Description: </p>
+ * <p>Company: www.iflytek.com</p>
+ * @author iflytek
+ */
+public class FinalResult extends Result {
+	
+	public int ret;
+	
+	public float total_score;
+	
+	@Override
+	public String toString() {
+		return "返回值：" + ret + "，总分：" + total_score;
+	}
+}
